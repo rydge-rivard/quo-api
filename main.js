36f9746ba3ac13f9fc75412a2a6c9e88d4bac838
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2025-11-14 12:05:57 +0100

    Get form values in JS function.

---
+++ main.js
@@ -0,0 +1,8 @@
+async function createContact() {
+  const apiKey = document.getElementById("apiKey").value.trim();
+  const firstName = document.getElementById("firstName").value.trim();
+  const lastName = document.getElementById("lastName").value.trim();
+  const phoneNumber = document.getElementById("phoneNumber").value.trim();
+  const API_BASE_URL =
+    "[https://api.openphone.com/v1/contacts](https://api.openphone.com/v1/contacts)";
+}
