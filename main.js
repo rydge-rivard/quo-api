bae296b17498798442799a5ae731019cd8b54e9c
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2025-11-14 12:25:33 +0100

    Format data payload for POST.

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 async function createContact() {
   const apiKey = document.getElementById("apiKey").value.trim();
-  const firstName = document.getElementById("firstName").value.trim();
-  const lastName = document.getElementById("lastName").value.trim();
+  const fName = document.getElementById("firstName").value.trim();
+  const lName = document.getElementById("lastName").value.trim();
   const phoneNumber = document.getElementById("phoneNumber").value.trim();
   const API_BASE_URL =
     "[https://api.openphone.com/v1/contacts](https://api.openphone.com/v1/contacts)";
 }
+
+const data = {
+  defaultFields: {
+    firstName: fName,
+    lastName: lName,
+    phoneNumbers: [
+      {
+        name: "primary", // Required structure
+        value: phoneNumber,
+      },
+    ],
+  },
+  source: "public-api",
+};
