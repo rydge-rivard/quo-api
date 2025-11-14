83a6ac5e48ea0d5a1946b145dda44652f934797c
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2025-11-14 12:31:59 +0100

    Attempt fix and deploy for testing.

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-async function createContact() {
+async function createContact(event) {
+  if (event) {
+    event.preventDefault();
+  }
   const apiKey = document.getElementById("apiKey").value.trim();
   const fName = document.getElementById("firstName").value.trim();
   const lName = document.getElementById("lastName").value.trim();
   const phoneNumber = document.getElementById("phoneNumber").value.trim();
   const API_BASE_URL =
     "[https://api.openphone.com/v1/contacts](https://api.openphone.com/v1/contacts)";
-}
 
-const data = {
-  defaultFields: {
-    firstName: fName,
-    lastName: lName,
-    phoneNumbers: [
-      {
-        name: "primary", // Required structure
-        value: phoneNumber,
-      },
-    ],
-  },
-  source: "public-api",
-};
+  const data = {
+    defaultFields: {
+      firstName: fName,
+      lastName: lName,
+      phoneNumbers: [
+        {
+          name: "primary", // Required structure
+          value: phoneNumber,
+        },
+      ],
+    },
+    source: "public-api",
+  };
+}
 
 try {
   const response = await fetch(API_BASE_URL, {
